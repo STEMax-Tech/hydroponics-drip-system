b816201b6a59ff3a3b444ca5037e9fe15a1b168d
NamVo <51428674+namvotech@users.noreply.github.com>
2024-10-27 09:49:53 +0700

    commit

--- main.ts
+++ main.ts
@@ -17,17 +17,12 @@ basic.forever(function () {
     ))
 })
 basic.forever(function () {
-    serial.writeLine("Soil Moisture: " + soilMoisture + " %")
-    serial.writeLine("Set Soil Moisture: " + setSoilMoisture + " %")
-    serial.writeLine("Set Motor Speed: " + setMotorSpeed + " %")
     lcd.displayText("Moisture:" + soilMoisture + "%    ", 1, 1)
     lcd.displayText("SM:" + setSoilMoisture + "  ", 1, 2)
     lcd.displayText("SP:" + setMotorSpeed + "  ", 9, 2)
     if (soilMoisture > setSoilMoisture) {
-        lcd.displayText(" ", 16, 1)
         basic.showIcon(IconNames.Happy)
     } else {
-        lcd.displayText(lcd.displaySymbol(lcd.Symbols.sym10), 16, 1)
         basic.showIcon(IconNames.Sad)
     }
     basic.pause(200)
@@ -50,9 +45,7 @@ basic.forever(function () {
 basic.forever(function () {
     if (soilMoisture > setSoilMoisture) {
         l9110.pauseMotor(l9110.Motor.MotorA)
-        l9110.pauseMotor(l9110.Motor.MotorB)
     } else {
         l9110.controlMotor(l9110.Motor.MotorA, l9110.Rotate.Forward, setMotorSpeed)
-        l9110.controlMotor(l9110.Motor.MotorB, l9110.Rotate.Forward, setMotorSpeed)
     }
 })
